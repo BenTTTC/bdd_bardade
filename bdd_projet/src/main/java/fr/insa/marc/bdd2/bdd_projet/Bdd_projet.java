ab81390c40bab594293aefa6cc41a822005e6374
bmarc01 <bmarc01@SEC-C212-F11.csip.in>
2023-10-17 08:54:08 +0200

    debut

---
+++ bdd_projet/src/main/java/fr/insa/marc/bdd2/bdd_projet/Bdd_projet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.marc.bdd2.bdd_projet;
+
+/**
+ *
+ * @author bmarc01
+ */
+public class Bdd_projet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
